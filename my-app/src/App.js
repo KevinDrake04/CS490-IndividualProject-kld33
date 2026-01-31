c0d47a5acfe59d262e1cc9792b3ae7c1e9d7dabe
Kevin Drake <kld33@njit.edu>
2026-01-31 01:48:38 -0500

    Created flask backend, with test connection

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 // src/App.js
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -7,6 +8,20 @@ import Customers from './components/Customers';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -25,6 +40,15 @@ function App() {
             </div>
           } />
         </Routes>
+          
+        {(typeof data.members === 'undefined') ? (
+          <p>...</p>
+        ): (
+          data.members.map((member, i)=>(
+            <p key={i}>{member}</p>
+          ))
+        )}
+        
       </div>
     </BrowserRouter>
   );
