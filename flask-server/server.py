2f34db9b8d5945dbc9d815cec27c6be7f43bc6a6
Kevin Drake <kld33@njit.edu>
2026-02-02 18:43:04 -0500

    Created vscode task to start the mysql80 service, and wrote the connector in the flask server.py script allowing for interaction with sakila database.

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
+
+con=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='password',
+    database='sakila'
+)
 
 #Members API route
 @app.route("/members")
