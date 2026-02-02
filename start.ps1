e0e308686a39dd9f2c9fec63745dfd2ba531f5d1
Kevin Drake <kld33@njit.edu>
2026-02-02 15:44:09 -0500

    synced for the first time and cleaned up unused files

--- start.ps1
+++
@@ -1,7 +0,0 @@
-$root = $PSScriptRoot
-
-$backend  = Join-Path $root "flask-server"
-$frontend = Join-Path $root "my-app"
-
-Start-Process powershell -WorkingDirectory $backend  -ArgumentList "-NoExit","-Command","py server.py"
-Start-Process powershell -WorkingDirectory $frontend -ArgumentList "-NoExit","-Command","npm start"
