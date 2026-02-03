3e914ac4acd35def48ec97bf30fa591dd6b3176a
Kevin Drake <kld33@njit.edu>
2026-02-02 20:35:58 -0500

    Cleaned up testing connections

--- flask-server/server.py
+++ flask-server/server.py
@@ -98,10 +98,5 @@ def get_customerDetails():
 
 ###############
 
-#Members API route
-@app.route("/members")
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]} 
-
 if __name__ == "__main__":
     app.run(debug=True)
